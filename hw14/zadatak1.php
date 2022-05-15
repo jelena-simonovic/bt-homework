10947cea45e907b6f345d22dd32018bccaec3242
jelena-simonovic <jelena.v412412@gmail.com>
2022-05-15 15:37:50 +0200

    hw14 zadatak1

---
+++ hw14/zadatak1.php
@@ -0,0 +1,22 @@
+<?php
+
+if (empty($_GET['day']) || !is_numeric($_GET['day'])) {
+    die('Day has not been properly set.');
+}
+if (empty($_GET['month']) || !is_numeric($_GET['month'])) {
+    die('Month has not been properly set.');
+}
+if (empty($_GET['year']) || !is_numeric($_GET['year'])) {
+    die('Year has not been properly set.');
+}
+
+$day = $_GET['day'];
+$month = $_GET['month'];
+$year = $_GET['year'];
+
+if (checkdate($month, $day, $year)) {
+    $date = $day . "/" . $month . "/" . $year;
+    echo "The date you chose is: $date.";
+} else {
+    echo "Not a valid date.";
+}
---
+++ hw14/zadatak2.php
@@ -0,0 +1,31 @@
+<?php
+
+
+function result($totalFuel, $fuelUseByTruck, $remainingFuelValue = null)
+{
+    if (!empty($remainingFuelValue)) {
+        if (($totalFuel > 0) && ($fuelUseByTruck > 0) && !is_string($totalFuel) && !is_string($fuelUseByTruck)) {
+            if ($fuelUseByTruck > $totalFuel) {
+                return "Not enough fuel for even one truck.";
+            } else {
+                $remainingFuel = $totalFuel % $fuelUseByTruck;
+                return "Remaining fuel: " . $remainingFuel . " liters.";
+            }
+        } else if (is_string($totalFuel) || is_string($fuelUseByTruck) || $totalFuel <= 0 || $fuelUseByTruck <= 0) {
+            die('Total fuel and fuel use values must be numbers over 0.');
+        }
+    } else {
+        if (($totalFuel > 0) && ($fuelUseByTruck > 0) && !is_string($totalFuel) && !is_string($fuelUseByTruck)) {
+            if ($fuelUseByTruck > $totalFuel) {
+                return "Not enough fuel for even one truck.";
+            } else {
+                $numberOFTrucks = floor($totalFuel / $fuelUseByTruck);
+                return "Enough fuel for: $numberOFTrucks trucks.";
+            }
+        } else if (is_string($totalFuel) || is_string($fuelUseByTruck) || $totalFuel <= 0 || $fuelUseByTruck <= 0) {
+            die('Total fuel and fuel use values must be numbers over 0.');
+        }
+    }
+}
+
+echo result(55, 15);
